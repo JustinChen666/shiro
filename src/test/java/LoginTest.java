6bffd051f2d56c4f229040f041266c39802ca283
ChenJun9401 <31572225+ChenJun9401@users.noreply.github.com>
2020-05-11 12:00:49 +0800

    授权操作

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -7,11 +7,13 @@ import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class LoginTest {
     @Test
     public void testLogin() {
         //创建安全管理器的工厂对象
-        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro-md5.ini");
+        IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro-permission.ini");
         //获取安全管理器
         SecurityManager securityManager = factory.getInstance();
         //设置到当前的环境当中
@@ -19,7 +21,7 @@ public class LoginTest {
         //获取subject
         Subject subject = SecurityUtils.getSubject();
         //创建登录令牌
-        UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "666");
+        UsernamePasswordToken token = new UsernamePasswordToken("lisi", "888");
         //执行认证操作
         try {
             subject.login(token);
@@ -32,9 +34,30 @@ public class LoginTest {
         }
 
         //认证状态
-        System.out.println(subject.isAuthenticated());
+        //System.out.println(subject.isAuthenticated());
+
+        //权限判断
+        //check的方式是没有返回结果,如果没有权限就抛出异常
+        //subject.checkPermission("user:create");
+        //subject.checkPermission("user:update");
+
+        //isPermitted有boolean类型的返回值
+        System.out.println(subject.isPermitted("user:create"));
+        System.out.println(subject.isPermitted("user:update"));
+        //把所有结果都放到一个数组中
+        System.out.println(Arrays.toString(subject.isPermitted("user:create", "user:update")));
+        //是否包含指定的所有权限,只要其它一个没有,都为false
+        System.out.println(subject.isPermittedAll("user:create","user:delete"));
+
+        //角色判断
+        //subject.checkRole("role1");
+        //subject.checkRole("role2");
+        System.out.println("==================");
+        System.out.println(subject.hasRole("role1"));
+        System.out.println(subject.hasRole("role2"));
+
         //注销
-        subject.logout();
-        System.out.println(subject.isAuthenticated());
+        //subject.logout();
+        //System.out.println(subject.isAuthenticated());
     }
 }
